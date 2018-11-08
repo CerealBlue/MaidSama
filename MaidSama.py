b4fd08d4d147e503c0e70749ec73127fc3b05c4f
mayank185T9 <43036566+mayank185T9@users.noreply.github.com>
2018-11-08 14:10:32 +0530

    Update MaidSama.py

--- MaidSama.py
+++ MaidSama.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 async def testo(ctx,  *args):
     print ("Testing Function")
     
-@bot.command(pass_context = True, aliases=['throwemout' ])
+@bot.command(pass_context = True, aliases=['throwout' ])
 async def kick(ctx, userName: discord.User):
 '''establishing a function kick'''
     await bot.kick(userName)
